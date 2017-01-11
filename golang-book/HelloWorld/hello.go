929c9e1f0c4dc182814bea117a43f5c5bc62e8c7
y2jinc <y2jinc@gmail.com>
2017-01-11 09:48:33 +0900

    * Start to study Go Language

---
+++ golang-book/HelloWorld/hello.go
@@ -0,0 +1,9 @@
+﻿package main
+ 
+import "fmt"
+ 
+// 이것은 주석이다
+ 
+func main() {
+    fmt.Println("Hello World")
+}
---
+++ golang-book/chapter4/variables.go
@@ -0,0 +1,45 @@
+﻿package main
+ 
+import "fmt"
+ 
+const x string = "Hello World"
+var (
+    a = 5
+    b = 10
+    c = 15
+)
+ 
+func f() {
+	x := 3
+    fmt.Println(x)
+}
+
+func input_test() {
+	fmt.Print("Enter a number: ")
+    var input float64
+    fmt.Scanf("%f", &input)
+
+	output := input * 2
+    fmt.Println(output)
+}
+
+func main() {
+	fmt.Println("-----------------------------")
+    fmt.Println(x)
+	
+	fmt.Println("-----------------------------")
+	f()
+
+	fmt.Println("-----------------------------")
+	fmt.Println("a :", a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+	fmt.Println("-----------------------------")
+	test_x := 5
+	test_x += 1
+	fmt.Println(test_x)
+
+	fmt.Println("-----------------------------")
+	input_test()
+}
