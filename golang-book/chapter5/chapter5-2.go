f11f8a3187ba41366aa8f76cc74553ac60dec4a3
y2jinc <y2jinc@gmail.com>
2017-01-31 15:30:49 +0900

    4장 변수 5장 제어구조 6장 배열 코드 추가.

---
+++ golang-book/chapter5/chapter5-2.go
@@ -0,0 +1,28 @@
+﻿package main
+ 
+import "fmt"
+ 
+func main() {
+    i := 1
+    for i <= 10 {
+		if i % 2 == 0 {
+			// 짝수
+			fmt.Println(i, "짝수")
+		} else {
+			// 홀수
+			fmt.Println(i, "홀수")
+		}
+
+		switch i {
+		case 0: fmt.Println("영")
+		case 1: fmt.Println("일")
+		case 2: fmt.Println("이")
+		case 3: fmt.Println("삼")
+		case 4: fmt.Println("사")
+		case 5: fmt.Println("오")
+		default: fmt.Println("알 수 없는 숫자")
+		}
+        
+        i = i + 1
+    }
+}
---
+++ golang-book/chapter5/chapter5.go
@@ -0,0 +1,11 @@
+﻿package main
+ 
+import "fmt"
+ 
+func main() {
+    i := 1
+    for i <= 10 {
+        fmt.Println(i)
+        i = i + 1
+    }
+}
